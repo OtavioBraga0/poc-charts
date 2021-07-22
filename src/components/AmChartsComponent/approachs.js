8dafedb3e4f6d16ba429b28033bc9770e09a2ed1
OtavioBraga0 <otavio.braga123@gmail.com>
2021-07-22 10:23:27 -0300

    fix hit on series

---
+++ src/components/AmChartsComponent/approachs.js
@@ -0,0 +1,59 @@
+/*
+ *
+ * PLOT BULLETS IN ALL POINT IN SERIES
+ *
+ */
+
+// series.dummyData = {...series.dummyData, selectedBullet: null}
+// chart.plotContainer.events.on("hit", ({target, point}) => {
+//     const dataItem = valueAxis.getSeriesDataItem(target.baseSprite.series.values[0], point)
+//     const bullet = series.bullets.values[0]._clones.getIndex(dataItem.component.tooltipDataItem.index)
+//     const {selectedBullet} = series.dummyData
+//     if (selectedBullet) selectedBullet.opacity = 0
+//     if (bullet) bullet.opacity = 1
+//     series.dummyData.selectedBullet = bullet
+// }, this)
+
+/*
+ *
+ * MOVE PLOT ON HIT
+ *
+ */
+// chart.plotContainer.events.on("hit", ({target, point}) => {
+//     const dataItem = valueAxis.getSeriesDataItem(target.baseSprite.series.values[0], point)
+//     if (series.bullets.values[0]) {
+//       const bullet = series.bullets.values[0]._clones.getIndex(dataItem.component.tooltipDataItem.index)
+//       if (bullet) {
+//         const {x, y} = bullet.group
+//         plot.moveTo({x, y})
+//       }
+//     }
+//   }, this)
+
+/*
+ *
+ * ON SCROLLBAR RANGE CHANGED
+ *
+ */
+
+// scrollbarX.events.on(
+//     "rangechanged",
+//     ({ target }: { target: any }) => {
+//       const dataItem = valueAxis.getSeriesDataItem(
+//         target.baseSprite.series.values[0],
+//         plot as any
+//       );
+//       if (dataItem && dataItem.component) {
+//         const bullet = series.bullets.values[0].clones.getIndex(
+//           dataItem.component.tooltipDataItem.index
+//         ) as am4charts.Bullet;
+
+//         if (bullet) {
+//           const { x, y } = bullet.group;
+
+//           plot.moveTo({ x, y });
+//         }
+//       }
+//     },
+//     this
+//   );
--- src/components/AmChartsComponent/index.jsx
+++ src/components/AmChartsComponent/index.jsx
@@ -1,204 +1,258 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
 
 import * as am4core from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
-import { highForecast, lowForecast, mockedData as data } from '../../data';
+import { highForecast, lowForecast, mockedData as data } from "../../data";
 
-import {colors} from '../constants'
+import { colors } from "../constants";
+import { useState } from "react";
 
 am4core.useTheme(am4themes_animated);
 
 export const AmChartsComponent = () => {
-    const amChart = useRef()
+  const amChart = useRef();
+  const [height, setHeight] = useState(800) 
 
-    const point = useRef()
+  useEffect(() => {
+    amChart.current = am4core.create(
+      "chartdiv",
+      am4charts.XYChart
+    );
 
-    useEffect(() => {
-        amChart.current = am4core.create("chartdiv", am4charts.XYChart)
-    }, [amChart])
+    return () => {
+      if (amChart.current) {
+        amChart.current.dispose();
+      }
+    };
+  }, [amChart]);
 
-    useEffect(() => {
-        if (amChart.current) {
-            const chart = amChart.current
-            
-            chart.paddingRight = 20;
-            chart.zoomOutButton.disabled = true;
+  useEffect(() => {
+    if (amChart.current) {
+      const chart = amChart.current;
 
-            const cursor = new am4charts.XYCursor();
-            cursor.lineX.disabled = false
-            cursor.lineY.disabled = true
-            cursor.behavior = "none"
+      if (chart) {
+        chart.zoomOutButton.disabled = true;
 
-            cursor.lineX.stroke = am4core.color(colors.grey500);
-            cursor.lineX.strokeWidth = 1;
-            cursor.lineX.strokeOpacity = 0.8;
-            cursor.lineX.strokeDasharray = "";
+        const cursor = new am4charts.XYCursor();
+        cursor.lineY.disabled = true;
+        cursor.behavior = "none";
 
-            chart.cursor = cursor
+        cursor.lineX.stroke = am4core.color(colors.grey500);
+        cursor.lineX.strokeWidth = 1;
+        cursor.lineX.strokeOpacity = 0.8;
 
-            const dateAxis = chart.xAxes.push(new am4charts.DateAxis())
-            dateAxis.renderer.grid.template.disabled = true
-            dateAxis.renderer.fontFamily = "'Roboto', Helvetica, Arial, sans-serif"
-            dateAxis.renderer.fontWeight = '700'
-            dateAxis.renderer.fontSize = 10
-            dateAxis.renderer.fontColor = am4core.color(colors.blue500)
-            
-            dateAxis.tooltip.label.fontFamily = "'Roboto', Helvetica, Arial, sans-serif"
-            dateAxis.tooltip.label.fill = am4core.color(colors.blue500)
-            dateAxis.tooltip.label.fontSize = 10
-            dateAxis.tooltip.background.fill = am4core.color(colors.sand500)
-            dateAxis.tooltip.stroke = am4core.color(colors.blue500)
-            dateAxis.tooltip.strokeWidth = 1
-            
-            const valueAxis = chart.yAxes.push(new am4charts.ValueAxis())
-            valueAxis.renderer.grid.template.disabled = true;
-            valueAxis.renderer.fontFamily = "'Roboto', Helvetica, Arial, sans-serif"
-            valueAxis.renderer.fontWeight = '700'
-            valueAxis.renderer.fontSize = 10
-            valueAxis.renderer.fontColor = am4core.color(colors.blue500)
-            valueAxis.tooltip.disabled = true;
-            valueAxis.strictMinMax = true
-            valueAxis.extraMin = 0.2;
-            valueAxis.extraMax = 0.2; 
-   
-            const series = new am4charts.LineSeries();
-            series.data = data
-            series.dataFields.valueY = "total";
-            series.dataFields.dateX = "date";
-            series.name = "series";
-            series.strokeWidth = 2;
-            series.stroke = am4core.color(colors.blue500)
-            series.fillOpacity = 1;
+        chart.cursor = cursor;
 
-            series.tooltipHTML = `<li>Past / Projected Data: <b>{total}</b></li>`
-            series.tooltip.getFillFromObject = false
-            series.tooltip.background.fill = am4core.color(colors.sand500)
-            series.tooltip.label.fill = am4core.color(colors.blue500)
-            series.tooltip.label.fontFamily = "'Roboto', Helvetica, Arial, sans-serif"
-            series.tooltip.label.fontSize = 12
-            series.tooltip.stroke = am4core.color(colors.blue500)
-            series.tooltip.strokeWidth = 2
+        const dateAxis = new am4charts.DateAxis();
+        dateAxis.tooltip.label.fontFamily =
+          "'Roboto', Helvetica, Arial, sans-serif";
+        dateAxis.tooltip.label.fill = am4core.color(colors.blue500);
+        dateAxis.tooltip.label.fontSize = 10;
+        dateAxis.tooltip.background.fill = am4core.color(colors.sand500);
+        dateAxis.tooltip.stroke = am4core.color(colors.blue500);
+        dateAxis.tooltip.strokeWidth = 1;
 
-            chart.series.push(series)
-            
-            const gradient = new am4core.LinearGradient()
-            gradient.rotation = 90
-            gradient.addColor(am4core.color(colors.green500))
-            gradient.addColor(am4core.color(colors.sand500))
+        const dateAxisRendered = chart.xAxes.push(dateAxis);
+        dateAxisRendered.renderer.grid.template.disabled = true;
+        dateAxisRendered.renderer.fontFamily =
+          "'Roboto', Helvetica, Arial, sans-serif";
+        dateAxisRendered.renderer.fontWeight = "700";
+        dateAxisRendered.renderer.fontSize = 10;
 
-            series.fill = gradient
+        const valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+        valueAxis.renderer.grid.template.disabled = true;
+        valueAxis.renderer.fontFamily =
+          "'Roboto', Helvetica, Arial, sans-serif";
+        valueAxis.renderer.fontWeight = "700";
+        valueAxis.renderer.fontSize = 10;
+        valueAxis.tooltip = false;
+        valueAxis.strictMinMax = true;
+        valueAxis.extraMin = 0.2;
+        valueAxis.extraMax = 0.2;
 
-            const range = valueAxis.createSeriesRange(series);
-            range.value = 0;
-            range.endValue = -10000000;
-            range.contents.stroke = am4core.color(colors.error);
+        valueAxis.getSeriesDataItem = function (series, position) {
+          const key = this.axisFieldName + this.axisLetter;
+          const value = this.positionToValue(position);
+          const dataItem = series.dataItems.getIndex(
+            series.dataItems.findClosestIndex(
+              value,
+              (x) => {
+                return x[key] ? x[key] : undefined;
+              },
+              "any"
+            )
+          );
+          return dataItem
+        };
 
-            const negativeGradient = new am4core.LinearGradient()            
-            negativeGradient.addColor(am4core.color(colors.sand500))
-            negativeGradient.addColor(am4core.color(colors.error))
-            range.contents.fill = negativeGradient;
+        const series = new am4charts.LineSeries();
+        series.data = data;
+        series.dataFields.valueY = "total";
+        series.dataFields.dateX = "date";
+        series.name = "series";
+        series.strokeWidth = 2;
+        series.stroke = am4core.color(colors.blue500);
+        series.fillOpacity = 1;
 
-            const forecastHighRangeSeries = new am4charts.LineSeries();
-            forecastHighRangeSeries.data = highForecast
-            forecastHighRangeSeries.dataFields.valueY = "total";
-            forecastHighRangeSeries.dataFields.dateX = "date";
-            forecastHighRangeSeries.name = "high forecast";
-            forecastHighRangeSeries.strokeWidth = 1;
-            forecastHighRangeSeries.stroke = am4core.color(colors.grey500)
-            forecastHighRangeSeries.tensionX = .7
-            chart.series.push(forecastHighRangeSeries)
+        series.tooltipHTML = `<li>Past / Projected Data: <b>{total}</b></li>`;
+        series.tooltip.getFillFromObject = false;
+        series.tooltip.background.fill = am4core.color(colors.sand500);
+        series.tooltip.label.fill = am4core.color(colors.blue500);
+        series.tooltip.label.fontFamily =
+          "'Roboto', Helvetica, Arial, sans-serif";
+        series.tooltip.label.fontSize = 12;
+        series.tooltip.stroke = am4core.color(colors.blue500);
+        series.tooltip.strokeWidth = 2;
 
-            const forecastLowRangeSeries = new am4charts.LineSeries();
-            forecastLowRangeSeries.data = lowForecast
-            forecastLowRangeSeries.dataFields.valueY = "total";
-            forecastLowRangeSeries.dataFields.dateX = "date";
-            forecastLowRangeSeries.name = "low forecast";
-            forecastLowRangeSeries.strokeWidth = 1;
-            forecastLowRangeSeries.stroke = am4core.color(colors.grey500)
-            forecastLowRangeSeries.tensionX = .7
-            chart.series.push(forecastLowRangeSeries)
+        chart.series.push(series);
 
-            const scrollbarX = new am4charts.XYChartScrollbar()
-            scrollbarX.series.push(series)
-            scrollbarX.series.push(forecastLowRangeSeries)
-            scrollbarX.series.push(forecastHighRangeSeries)
-            scrollbarX.end = .45
-            scrollbarX.start =  .55
-            scrollbarX.thumb.background.fill = am4core.color(colors.green500)
-            scrollbarX.unselectedOverlay.fill = am4core.color("#fff");
-            scrollbarX.unselectedOverlay.fillOpacity = .8;
+        const gradient = new am4core.LinearGradient();
+        gradient.rotation = 90;
+        gradient.addColor(am4core.color(colors.green500));
+        gradient.addColor(am4core.color(colors.sand500));
 
-            chart.scrollbarX = scrollbarX
-            chart.scrollbarX.parent = chart.bottomAxesContainer
+        series.fill = gradient;
 
-            const {startGrip, endGrip} = chart.scrollbarX
+        const forecastHighRangeSeries = new am4charts.LineSeries();
+        forecastHighRangeSeries.data = highForecast;
+        forecastHighRangeSeries.dataFields.valueY = "total";
+        forecastHighRangeSeries.dataFields.dateX = "date";
+        forecastHighRangeSeries.name = "high forecast";
+        forecastHighRangeSeries.strokeWidth = 1;
+        forecastHighRangeSeries.stroke = am4core.color(colors.grey500);
+        forecastHighRangeSeries.tensionX = 0.7;
+        chart.series.push(forecastHighRangeSeries);
 
-            startGrip.background.disabled = true;
-            endGrip.background.disabled = true;
-            
-            const startGripImg = startGrip.createChild(am4core.Rectangle);
-            startGripImg.width = 10;
-            startGripImg.height = 15;
-            startGripImg.fill = am4core.color(colors.sand500);
-            startGripImg.align = "center";
-            startGripImg.valign = "middle";
-            startGripImg.zIndex = 2
-            startGripImg.strokeWidth = 1
-            startGripImg.stroke = am4core.color(colors.blue500)
+        const forecastLowRangeSeries = new am4charts.LineSeries();
+        forecastLowRangeSeries.data = lowForecast;
+        forecastLowRangeSeries.dataFields.valueY = "total";
+        forecastLowRangeSeries.dataFields.dateX = "date";
+        forecastLowRangeSeries.name = "low forecast";
+        forecastLowRangeSeries.strokeWidth = 1;
+        forecastLowRangeSeries.stroke = am4core.color(colors.grey500);
+        forecastLowRangeSeries.tensionX = 0.7;
+        chart.series.push(forecastLowRangeSeries);
 
-            // Add vertical bar
-            const startLine = startGrip.createChild(am4core.Rectangle);
-            startLine.height = 60;
-            startLine.width = 1;
-            startLine.fill = am4core.color(colors.grey500);
-            startLine.align = "center";
-            startLine.valign = "middle";
-            startLine.zIndex = 1
+        const scrollbarX = new am4charts.XYChartScrollbar();
+        scrollbarX.series.push(series);
+        scrollbarX.series.push(forecastLowRangeSeries);
+        scrollbarX.series.push(forecastHighRangeSeries);
+        scrollbarX.end = 0.45;
+        scrollbarX.start = 0.55;
+        scrollbarX.thumb.background.fill = am4core.color(colors.green500);
+        scrollbarX.unselectedOverlay.fill = am4core.color("#fff");
+        scrollbarX.unselectedOverlay.fillOpacity = 0.8;
 
-            const endGripImg = endGrip.createChild(am4core.Rectangle);
-            endGripImg.width = 10;
-            endGripImg.height = 15;
-            endGripImg.fill = am4core.color(colors.sand500);
-            endGripImg.align = "center";
-            endGripImg.valign = "middle";
-            endGripImg.zIndex = 2
-            endGripImg.strokeWidth = 1
-            endGripImg.stroke = am4core.color(colors.blue500)
+        chart.scrollbarX = scrollbarX;
+        chart.scrollbarX.parent = chart.bottomAxesContainer;
 
-            // Add vertical bar
-            const endLine = endGrip.createChild(am4core.Rectangle);
-            endLine.height = 60;
-            endLine.width = 1;
-            endLine.fill = am4core.color(colors.grey500);
-            endLine.align = "center";
-            endLine.valign = "middle";
-            endLine.zIndex = 1
-            
-            const plot = series.createChild(am4charts.CircleBullet)
-            plot.stroke = am4core.color(colors.blue500)
-            plot.fill = am4core.color(colors.sand500)
-            plot.isMeasured = false
-            
-            series.segments.template.interactionsEnabled = true
-            series.segments.template.events.on("hit", ({target}) => {
-                const {dataContext, point} = target.dataItem.component.tooltipDataItem;
-                plot.moveTo(point)
-                point.current = dataContext
-            },this);
+        const { startGrip, endGrip } = chart.scrollbarX;
 
-            series.segments.template.events.on("drag", ({target }) => {
-                target.moveTo({x: 0, y: 0})
-                const data = target.dataItem.component.tooltipDataItem.dataContext;
-                point.current = data
-            },this)
-        }
-    }, [amChart])
+        startGrip.background.disabled = true;
+        endGrip.background.disabled = true;
+
+        const startGripImg = startGrip.createChild(am4core.Rectangle);
+        startGripImg.width = 10;
+        startGripImg.height = 15;
+        startGripImg.fill = am4core.color(colors.sand500);
+        startGripImg.align = "center";
+        startGripImg.valign = "middle";
+        startGripImg.zIndex = 2;
+        startGripImg.strokeWidth = 1;
+        startGripImg.stroke = am4core.color(colors.blue500);
+
+        // Add vertical bar
+        const startLine = startGrip.createChild(am4core.Rectangle);
+        startLine.height = 60;
+        startLine.width = 1;
+        startLine.fill = am4core.color(colors.grey500);
+        startLine.align = "center";
+        startLine.valign = "middle";
+        startLine.zIndex = 1;
+
+        const endGripImg = endGrip.createChild(am4core.Rectangle);
+        endGripImg.width = 10;
+        endGripImg.height = 15;
+        endGripImg.fill = am4core.color(colors.sand500);
+        endGripImg.align = "center";
+        endGripImg.valign = "middle";
+        endGripImg.zIndex = 2;
+        endGripImg.strokeWidth = 1;
+        endGripImg.stroke = am4core.color(colors.blue500);
+
+        // Add vertical bar
+        const endLine = endGrip.createChild(am4core.Rectangle);
+        endLine.height = 60;
+        endLine.width = 1;
+        endLine.fill = am4core.color(colors.grey500);
+        endLine.align = "center";
+        endLine.valign = "middle";
+        endLine.zIndex = 1;
+
+        const plot = series.createChild(am4charts.CircleBullet);
+        plot.stroke = am4core.color(colors.blue500);
+        plot.width = 14;
+        plot.fill = am4core.color(colors.sand500);
+
+        // const arrowRight = plot.createChild(am4charts.CircleBullet)
+        // arrowRight.fill = am4core.color("white")
+        // arrowRight.strokeWidth = 0
+        // arrowRight.width = 16
+        // arrowRight.dx = 23
+        // arrowRight.align = "center"
+        // arrowRight.valign = "middle"
+
+        // const arrowRightImage = arrowRight.createChild(am4core.Image)
+        // arrowRightImage.path = "M0.999969 7L3.99997 4L0.999969 1"
+        // arrowRightImage.stroke = am4core.color(colors.blue500)
+
+        // const arrowLeft = plot.createChild(am4charts.CircleBullet)
+        // arrowLeft.fill = am4core.color("white")
+        // arrowLeft.strokeWidth = 0
+        // arrowLeft.width = 16
+        // arrowLeft.dx = -23
+
+        // const arrowLeftImage = arrowLeft.createChild(am4core.Image)
+        // arrowLeftImage.path = "M0.999969 7L3.99997 4L0.999969 1"
+        // arrowLeftImage.stroke = am4core.color(colors.blue500)`
+
+        series.bullets.create(am4charts.Bullet)
+        chart.maskBullets = false;
+
+        chart.plotContainer.events.on("hit", ({target, point}) => {
+          const dataItem = valueAxis.getSeriesDataItem(target.baseSprite.series.values[0], point)
+          if (series.bullets.values[0]) {
+            const bullet = series.bullets.values[0]._clones.getIndex(dataItem.component.tooltipDataItem.index)
+            if (bullet) {
+              console.log(bullet);
+              const {x, y} = bullet
+              plot.moveTo({x, y})
+            }
+          }
+        }, this)
+
+        scrollbarX.events.on("positionchanged", ({ target }) => {
+          const dataItem = valueAxis.getSeriesDataItem(target.series.values[0], plot)
+          if (series.bullets.values[0]) {
+            const bullet = series.bullets.values[0]._clones.getIndex(dataItem.component.tooltipDataItem.index)
+            if (bullet) {
+              console.log(bullet);
+              const {x, y} = bullet
+              plot.moveTo({x, y})
+            }
+          }
+        },this);
+      }
+    }
+  }, [amChart]);
 
   return (
-      <>
-        <div id="chartdiv" style={{height: 600 }}/>
-        <button type="button" onClick={() => console.log('point', point.current)}>Click!</button>
-      </>
-  )
-}
+    <>
+      <div id="chartdiv" style={{ height }} />
+      <button onClick={() => setHeight(prev => prev === 500 ? 800 : 500)}>
+        Toggle Height
+      </button>
+    </>
+  );
+};
